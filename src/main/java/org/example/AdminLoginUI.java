7aca489ec280812aceaa27b1a10bddae31594353
dazz10 <dazzdebanjan10@outlook.com>
2025-12-27 14:06:42 +0530

    Login functionality added

--- src/main/java/org/example/AdminLoginUI.java
+++ src/main/java/org/example/AdminLoginUI.java
@@ -1,4 +1,28 @@
 package org.example;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class AdminLoginUI {
+    public static void main(String[] args) {
+        String username = "admin";
+        String pass = "admin123";
+
+        adminLogin(username, pass);
+
+
+    }
+    public static void adminLogin(String username, String pass) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String usr = sc.nextLine();
+        System.out.println("Enter password: ");
+        String pas = sc.nextLine();
+
+        if(Objects.equals(usr, username) && Objects.equals(pass, pas)) {
+            System.out.println("Welcome to SMS");
+        } else  {
+            System.out.println("Invalid credentials, please try again");
+        }
+    }
 }
